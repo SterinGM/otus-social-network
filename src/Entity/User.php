eae04a88a1b71c41cf7da064e778040d37886e85
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-06-30 00:21:06 +0300

    update user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -25,6 +27,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private string $password;
+
+    #[ORM\Column(length: 50)]
+    private string $firstName;
+
+    #[ORM\Column(length: 50)]
+    private string $secondName;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    private DateTimeImmutable $birthdate;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private string $biography = '';
+
+    #[ORM\Column(length: 100)]
+    private string $city;
 
     public function getId(): string
     {
@@ -66,7 +83,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see PasswordAuthenticatedUserInterface
      */
-    public function getPassword(): ?string
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -83,5 +100,65 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials(): void
     {
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getSecondName(): string
+    {
+        return $this->secondName;
+    }
+
+    public function setSecondName(string $secondName): static
+    {
+        $this->secondName = $secondName;
+
+        return $this;
+    }
+
+    public function getBirthdate(): DateTimeImmutable
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(DateTimeImmutable $birthdate): static
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    public function getBiography(): string
+    {
+        return $this->biography;
+    }
+
+    public function setBiography(string $biography): static
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
     }
 }
