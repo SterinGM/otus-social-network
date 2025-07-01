5c0bb2f964b0f68c596020a32755be56840b824c
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-07-02 00:32:59 +0300

    api token entity

---
+++ src/Repository/ApiTokenRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ApiToken;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ApiToken>
+ *
+ * @method ApiToken|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ApiToken|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ApiToken[]    findAll()
+ * @method ApiToken[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ApiTokenRepository extends ServiceEntityRepository
+{
+    private const string DATE_FORMAT = 'Y-m-d H:i:s';
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ApiToken::class);
+    }
+
+    public function create(ApiToken $token): void
+    {
+        $sql = 'INSERT INTO api_token(token, user_id, created_at)
+            VALUES(:token, :user_id, :created_at)';
+
+        $statement = $this->connection->prepare($sql);
+        $statement->bindValue('token', $token->getToken());
+        $statement->bindValue('user_id', $token->getUserId());
+        $statement->bindValue('created_at', $token->getCreatedAt()->format(self::DATE_FORMAT));
+        $statement->executeQuery();
+    }
+}
