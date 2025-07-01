93a788e16e17a50a55a3ee91aeaeb40fa1e15b2b
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-07-02 01:39:25 +0300

    added validation

--- src/Controller/API/User/GetController.php
+++ src/Controller/API/User/GetController.php
@@ -6,22 +6,38 @@ use App\DTO\User\Response\GetResponse;
 use App\Service\ApiJsonResponse;
 use App\Service\User\Profile;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class GetController
 {
     private Profile $profile;
+    private ValidatorInterface $validator;
 
-    public function __construct(Profile $profile)
+    public function __construct(Profile $profile, ValidatorInterface $validator)
     {
         $this->profile = $profile;
+        $this->validator = $validator;
     }
 
     #[Route('/user/get/{id}', name: 'api_user_get', methods: ['GET'])]
     public function __invoke(string $id): JsonResponse
     {
+        $this->validate($id);
+
         $user = $this->profile->getProfile($id);
 
         return ApiJsonResponse::create(GetResponse::createFromUser($user));
+    }
+
+    private function validate(string $id): void
+    {
+        $errors = $this->validator->validate($id, new Assert\Uuid());
+
+        if ($errors->count()) {
+            throw new BadRequestHttpException();
+        }
     }
 }
