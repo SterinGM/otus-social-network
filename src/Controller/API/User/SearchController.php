df7502fef7e5eec3b26e6217ae3b97fb52abb41a
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-07-26 16:52:41 +0300

    added search controller

---
+++ src/Controller/API/User/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\API\User;
+
+use App\DTO\User\Request\SearchRequest;
+use App\DTO\User\Response\SearchResponse;
+use App\Service\ApiJsonResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SearchController
+{
+    #[Route('/user/search', name: 'api_user_search', methods: ['GET'])]
+    public function __invoke(SearchRequest $registerRequest): JsonResponse
+    {
+        return ApiJsonResponse::create(SearchResponse::createFromResult([]));
+    }
+}
---
+++ src/DTO/User/Resolver/SearchResolver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DTO\User\Resolver;
+
+use App\DTO\User\Request\SearchRequest;
+use App\Service\ErrorSystem\Errors;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class SearchResolver implements ValueResolverInterface
+{
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    {
+        if (SearchRequest::class !== $argument->getType()) {
+            return;
+        }
+
+        $data = $request->getPayload()->all();
+        $errors = $this->validate($data);
+
+        if (count($errors)) {
+            $request->attributes->set('errors', $errors);
+
+            throw new BadRequestHttpException();
+        }
+
+        yield SearchRequest::createFromArray($data);
+    }
+
+    private function validate(array $data): array
+    {
+        $constraint = new Assert\Collection(fields: [
+            SearchRequest::FIELD_FIRST_NAME => new Assert\Length(min: 1, max: 50),
+            SearchRequest::FIELD_LAST_NAME => new Assert\Length(min: 1, max: 50),
+        ]);
+
+        $violations = $this->validator->validate($data, $constraint);
+
+        return Errors::toArray($violations);
+    }
+}
