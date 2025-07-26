dd0b7b16305f80e65e1418a15ec267f83a8ce60d
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-07-26 17:53:30 +0300

    added search service

--- src/Controller/API/User/SearchController.php
+++ src/Controller/API/User/SearchController.php
@@ -5,14 +5,24 @@ namespace App\Controller\API\User;
 use App\DTO\User\Request\SearchRequest;
 use App\DTO\User\Response\SearchResponse;
 use App\Service\ApiJsonResponse;
+use App\Service\Search\SearchInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SearchController
 {
+    private SearchInterface $search;
+
+    public function __construct(SearchInterface $search)
+    {
+        $this->search = $search;
+    }
+
     #[Route('/user/search', name: 'api_user_search', methods: ['GET'])]
     public function __invoke(SearchRequest $searchRequest): JsonResponse
     {
-        return ApiJsonResponse::create(SearchResponse::createFromResult([]));
+        $result = $this->search->searchUsers($searchRequest);
+
+        return ApiJsonResponse::create(SearchResponse::createFromResult($result)->users);
     }
 }
