db56a75c1e18a59caca9afac089f4326bb37743f
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-08-02 11:37:03 +0300

    fixed login service

--- src/Service/Security/Login.php
+++ src/Service/Security/Login.php
@@ -7,6 +7,7 @@ use App\Entity\ApiToken;
 use App\Repository\ApiTokenRepository;
 use App\Repository\UserRepository;
 use App\Service\Exception\InvalidCredentialsException;
+use App\Service\Exception\UserNotFoundException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Uid\Uuid;
 
@@ -29,6 +30,10 @@ class Login
     public function loginUser(LoginRequest $loginRequest): ApiToken
     {
         $user = $this->userRepository->getById($loginRequest->userId);
+
+        if ($user === null) {
+            throw new UserNotFoundException($loginRequest->userId);
+        }
 
         if (!$this->hasher->isPasswordValid($user, $loginRequest->password)) {
             throw new InvalidCredentialsException();
