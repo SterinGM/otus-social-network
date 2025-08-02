7f9b6928b8ac9dea6b8df614a7b4750bbe141746
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-08-02 16:10:43 +0300

    set user friend logic

---
+++ src/Repository/FriendRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FriendRepository
+{
+    private Connection $connection;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->connection = $entityManager->getConnection();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function setFriends(string $userSource, string $userTarget): void
+    {
+        $sql = 'INSERT INTO user_user(user_source, user_target) VALUES(:user_source, :user_target)
+            ON DUPLICATE KEY UPDATE user_target = user_target';
+
+        $statement = $this->connection->prepare($sql);
+        $statement->bindValue('user_source', $userSource);
+        $statement->bindValue('user_target', $userTarget);
+        $statement->executeQuery();
+    }
+}
--- src/Service/Friend/Friend.php
+++ src/Service/Friend/Friend.php
@@ -3,15 +3,18 @@
 namespace App\Service\Friend;
 
 use App\DTO\Friend\Request\SetRequest;
+use App\Repository\FriendRepository;
 use App\Repository\UserRepository;
 
 class Friend implements FriendInterface
 {
     private UserRepository $userRepository;
+    private FriendRepository $friendRepository;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, FriendRepository $friendRepository)
     {
         $this->userRepository = $userRepository;
+        $this->friendRepository = $friendRepository;
     }
 
     public function setUserFriend(SetRequest $setRequest): void
@@ -25,5 +28,7 @@ class Friend implements FriendInterface
         if ($user === null) {
             return;
         }
+
+        $this->friendRepository->setFriends($setRequest->fromUserId, $setRequest->userId);
     }
 }
