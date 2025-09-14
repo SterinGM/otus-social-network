60e8d878b21b53689f00c412df0d5306307ee3d7
Grigoriy Sterin <grigoriysterin@gmail.com>
2025-09-15 01:37:06 +0300

    updated dialog service

--- src/Repository/Dialog/ChatRepository.php
+++ src/Repository/Dialog/ChatRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository\Dialog;
 
 use App\Entity\Dialog\Chat;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Connection;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -11,8 +12,45 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class ChatRepository extends ServiceEntityRepository
 {
+    private Connection $connection;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Chat::class);
+
+        $this->connection = $this->getEntityManager()->getConnection();
+    }
+
+    public function getChatByUsers(string $userId1, string $userId2): ?Chat
+    {
+        $sql = 'SELECT * FROM chat WHERE user_ids = :user_ids';
+
+        $statement = $this->connection->prepare($sql);
+        $statement->bindValue('user_ids', [$userId1, $userId2]);
+        $result = $statement->executeQuery();
+
+        if (!$result->rowCount()) {
+            return null;
+        }
+
+        return $this->mapChat($result->fetchAssociative());
+    }
+
+    public function createChat(Chat $chat): void
+    {
+        $sql = 'INSERT INTO chat(id, user_ids)
+            VALUES(:id, :user_ids, :text, :created_at)';
+
+        $statement = $this->connection->prepare($sql);
+        $statement->bindValue('id', $chat->getId());
+        $statement->bindValue('user_ids', $chat->getUserIds());
+        $statement->executeStatement();
+    }
+
+    private function mapChat(array $data): Chat
+    {
+        return new Chat()
+            ->setId($data['id'])
+            ->setUserIds($data['userIds']);
     }
 }
--- src/Service/Dialog/Dialog.php
+++ src/Service/Dialog/Dialog.php
@@ -3,12 +3,57 @@
 namespace App\Service\Dialog;
 
 use App\DTO\Dialog\Request\SendRequest;
+use App\Entity\Dialog\Chat;
+use App\Repository\Dialog\ChatRepository;
+use App\Repository\Dialog\MessageRepository;
+use App\Repository\Main\UserRepository;
+use Symfony\Component\Uid\Uuid;
 
 class Dialog implements DialogInterface
 {
+    private UserRepository $userRepository;
+    private ChatRepository $chatRepository;
+    private MessageRepository $messageRepository;
+
+    public function __construct(
+        UserRepository    $userRepository,
+        ChatRepository    $chatRepository,
+        MessageRepository $messageRepository,
+    ) {
+        $this->userRepository = $userRepository;
+        $this->chatRepository = $chatRepository;
+        $this->messageRepository = $messageRepository;
+    }
 
     public function sendMessage(SendRequest $sendRequest): void
     {
-        // TODO: Implement sendMessage() method.
+        if ($sendRequest->userId === $sendRequest->fromUserId) {
+            return;
+        }
+
+        $user = $this->userRepository->getById($sendRequest->userId);
+
+        if ($user === null) {
+            return;
+        }
+
+        $chat = $this->getChat($sendRequest->fromUserId, $sendRequest->userId);
+
+        dump($chat);
+    }
+
+    private function getChat(string $userId1, string $userId2): Chat
+    {
+        $chat = $this->chatRepository->getChatByUsers($userId1, $userId2);
+
+        if ($chat == null) {
+            $chat = new Chat()
+                ->setId(Uuid::v7()->toRfc4122())
+                ->setUserIds([$userId1, $userId2]);
+
+            $this->chatRepository->createChat($chat);
+        }
+
+        return $chat;
     }
 }
